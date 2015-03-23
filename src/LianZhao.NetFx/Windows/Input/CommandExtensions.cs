3e374138a6234d79359645247434526958fe9962
lianzhao <knife.fe@gmail.com>
2015-03-23 17:52:03 +0800

    commands

---
+++ src/LianZhao.NetFx/Windows/Input/CommandExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Input;
+
+namespace LianZhao.Windows.Input
+{
+    public static class CommandExtensions
+    {
+        public static bool TryExecute(this ICommand command, object parameter)
+        {
+            if (command.CanExecute(parameter))
+            {
+                command.Execute(parameter);
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool TryExecute(this ICommand command)
+        {
+            return command.TryExecute(null);
+        }
+
+        public static void Execute(this ICommand command)
+        {
+            command.Execute(null);
+        }
+
+        public static bool CanExecute(this ICommand command)
+        {
+            return command.CanExecute(null);
+        }
+    }
+}
---
+++ src/LianZhao.NetFx/Windows/Input/DelegateCommand.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace LianZhao.Windows.Input
+{
+    public class DelegateCommand<T> : ICommand
+    {
+        private readonly Predicate<T> _canExecuteFunc;
+        private readonly Action<T> _executeAction;
+
+        public DelegateCommand(Action<T> executeAction, Predicate<T> canExecuteFunc = null)
+        {
+            if (executeAction == null)
+            {
+                throw new ArgumentNullException("executeAction");
+            }
+
+            _canExecuteFunc = canExecuteFunc;
+            _executeAction = executeAction;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecuteFunc == null)
+            {
+                return true;
+            }
+
+            if (parameter is T || parameter == null)
+            {
+                return _canExecuteFunc.Invoke((T)parameter);
+            }
+
+            return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeAction.Invoke((T)parameter);
+        }
+
+        public void RaiseCanExecuteChangedEvent()
+        {
+            OnCanExecuteChanged();
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        protected virtual void OnCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+    }
+}
