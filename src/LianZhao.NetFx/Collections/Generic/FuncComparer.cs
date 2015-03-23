124148d99540e9bfbc6fa88f9a286744c6581f4f
lianzhao <knife.fe@gmail.com>
2015-03-23 11:56:28 +0800

    comparer

---
+++ src/LianZhao.NetFx/Collections/Generic/FuncComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LianZhao.Collections.Generic
+{
+    public class FuncComparer<T> : Comparer<T>
+    {
+        public Func<T, T, int> Comparer { get; private set; }
+
+        public FuncComparer(Func<T, T, int> comparer)
+        {
+            Comparer = comparer;
+        }
+
+        public override int Compare(T x, T y)
+        {
+            return Comparer.Invoke(x, y);
+        }
+    }
+}
---
+++ src/LianZhao.NetFx/Collections/Generic/FuncEqualityComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LianZhao.Collections.Generic
+{
+    public class FuncEqualityComparer<T> : EqualityComparer<T>
+    {
+        public Func<T, T, bool> Comparer { get; private set; }
+
+        public FuncEqualityComparer(Func<T, T, bool> comparer)
+        {
+            Comparer = comparer;
+        }
+
+        public override bool Equals(T x, T y)
+        {
+            return Comparer.Invoke(x, y);
+        }
+
+        public override int GetHashCode(T obj)
+        {
+            return obj == null ? 0 : obj.GetHashCode();
+        }
+    }
+}
