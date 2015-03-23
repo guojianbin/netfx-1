0dd8facb9162ace008a7f087781e445d0bcb9f50
lianzhao <knife.fe@gmail.com>
2015-03-23 17:40:45 +0800

    lookup

---
+++ src/LianZhao.NetFx/Linq/Lookup.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace LianZhao.Linq
+{
+    public static class Lookup
+    {
+        #region Empty
+
+        public static ILookup<TKey, TValue> Empty<TKey, TValue>()
+        {
+            return EmptyHolder<TKey, TValue>.Instance;
+        }
+
+        private static class EmptyHolder<TKey, TValue>
+        {
+            public static readonly ILookup<TKey, TValue> Instance =
+                System.Linq.Enumerable.Empty<int>().ToLookup(x => default(TKey), x => default(TValue));
+        }
+        #endregion
+    }
+}
