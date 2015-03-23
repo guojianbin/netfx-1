3eb22b153fc60f87f9d83b3b8dc1d681421e7174
lianzhao <knife.fe@gmail.com>
2015-03-23 17:44:07 +0800

    ArrayExtensions

---
+++ src/LianZhao.NetFx/ArrayExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace LianZhao
+{
+    public static class ArrayExtensions
+    {
+        public static IEnumerable<T[]> ToEnumerable<T>(this T[,] array)
+        {
+            var rowCount = array.GetLength(0);
+            var lengthPerRow = array.GetLength(1);
+            for (var i = 0; i < rowCount; i++)
+            {
+                var row = new T[lengthPerRow];
+                for (var j = 0; j < lengthPerRow; j++)
+                {
+                    row[j] = array[i, j];
+                }
+
+                yield return row;
+            }
+        }
+    }
+}
